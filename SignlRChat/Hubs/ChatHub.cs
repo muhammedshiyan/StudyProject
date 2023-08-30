890294b9996ca091be21e74e0832776bea2c040c
shiyan <muhammedshiyanv@gmail.com>
2023-08-30 17:26:12 +0530

    r

--- SignlRChat/Hubs/ChatHub.cs
+++ SignlRChat/Hubs/ChatHub.cs
@@ -5,56 +5,103 @@ using System.Collections.Concurrent;
 using Microsoft.AspNetCore.Razor.Language.Intermediate;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
+using static System.Net.Mime.MediaTypeNames;
+using NuGet.Configuration;
 
 namespace SignlRChat.Hubs
 {
   
     public class ChatHub:Hub
     {
-        //public async Task NewTabOpened()
-        //{
-        //    await Clients.Others.SendAsync("TabOpened");
-        //}
-      
-
-
-        //public override Task OnConnectedAsync()
-        //{
-        //    _ = Groups.AddToGroupAsync(Context.ConnectionId, Context.User.Identity.Name);
-        //    return base.OnConnectedAsync();
-        //}
-      
+        private static int MessageCount = 0;
+        private static int ConnectedDevices = 0;
+        private static Dictionary<string, string> UserConnections = new Dictionary<string, string>();
+        private static HashSet<string> ConnectedUserIds = new HashSet<string>();
+        private CancellationToken newLikeCount;
+        private CancellationToken newDislikeCount;
 
         public async Task SendMessage(string user, string message)
-        {      
-            
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        {
+            MessageCount++;
+            await Clients.All.SendAsync("ReceiveMessage", user, message, MessageCount);
+
 
         }
 
 
 
-        public async Task Sendurltoall(string user, string message)
+        public async Task Sendurltoall(string image, string video, string text)
         {
 
-            await Clients.All.SendAsync("Receiveurl", user, message);
+            MessageCount++;
+            string userId = Context.User.Identity.Name;
+
+            await Clients.All.SendAsync("Receiveurl",image,video,text, userId);
+
+
 
         }
 
 
 
-        //public async Task SendVideo(string user, string videoUrl)
-        //{
-        //    await Clients.All.SendAsync("ReceiveVideo", user, videoUrl);
-        //}
 
-        public Task SendMessageToGroup(string user, string receiver, string message)
+        public override async Task OnConnectedAsync()
         {
-          
-            return Clients.Group(receiver).SendAsync("ReceiveMessage", user, message);
+            ConnectedDevices++;
 
+            // Update the count for all clients
+            await Clients.All.SendAsync("UpdateConnectedDevicesCount", ConnectedDevices);
+            if (Context.User.Identity.IsAuthenticated)
+            {
+                string userId = Context.User.Identity.Name;
+                ConnectedUserIds.Add(userId);
+                await Clients.All.SendAsync("UserConnected", $"{userId} connected.");
+            }
+            await base.OnConnectedAsync();
         }
 
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            ConnectedDevices--;
+
+            // Update the count for all clients
+            await Clients.All.SendAsync("UpdateConnectedDevicesCount", ConnectedDevices);
+
+            if (Context.User.Identity.IsAuthenticated)
+            {
+                string userId = Context.User.Identity.Name;
+                ConnectedUserIds.Remove(userId);
+                await Clients.All.SendAsync("UserDisconnected", $"{userId} disconnected.");
+            }
+            await base.OnDisconnectedAsync(exception);
+        }
+        public List<string> GetConnectedUserIds()
+        {
+            return ConnectedUserIds.ToList();
+        }
+
+        public async Task SendMessageTospecificUser(string userId, string message)
+        {
+            MessageCount++;
+            string sender = Context.User.Identity.Name;
+            //  await Clients.Client(userId).SendAsync("ReceiveMessagetospecificuser", userId, sender, message);
+            //  await Clients.User(userId).SendAsync("ReceiveMessagetospecificuser", userId, sender, message);
+            await Clients.All.SendAsync("ReceiveMessage", userId, message, MessageCount);
+        }
+
+        public async Task LikeMessage(int messageId)
+        {
+            // Update like count for the message (in memory or database)
+            // Broadcast updated like count to all clients
+            await Clients.All.SendAsync("UpdateMessageLikes", messageId, newLikeCount);
+        }
+
+        public async Task DislikeMessage(int messageId)
+        {
+            // Update dislike count for the message (in memory or database)
+            // Broadcast updated dislike count to all clients
+            await Clients.All.SendAsync("UpdateMessageDislikes", messageId, newDislikeCount);
+        }
 
 
 
--- SignlRChat/wwwroot/js/chat.js
+++ SignlRChat/wwwroot/js/chat.js
@@ -5,7 +5,7 @@
 
 const connection = new signalR.HubConnectionBuilder().withUrl("/chathub").configureLogging(signalR.LogLevel.Debug).withAutomaticReconnect().build();
 
-
+const connectedUsers = [];
 
 connection.onreconnecting(error => {
     console.assert(connection.state === signalR.HubConnectionState.Reconnecting);
@@ -23,28 +23,22 @@ document.getElementById("sendButton").disabled = true;
 
 
 
-connection.on("ReceiveMessage", function (user, message) {
+connection.on("ReceiveMessage", function (user, message, messageCount) {
 
-
+    document.getElementById("message-count").innerText = `Message Count: ${messageCount}`;
 
     console.log('message Received ');
 
     var li = document.createElement("li");
     document.getElementById("messagesList").appendChild(li);
 
-    li.textContent = `${user} says ${message}`;
+    li.textContent = `${user}  says ${message}`;
 
     console.log('message Received completed ');
 });
 
 
 
-//connection.on("ReceiveVideo", (user, videoUrl) => {
-   
-//    const receivedVideoElement = document.getElementById("receivedVideo");
-//    receivedVideoElement.src = videoUrl;
-//    receivedVideoElement.play();
-//});
 
 
 
@@ -65,7 +59,7 @@ connection.start().then(function () {
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
- 
+
     console.log('before invoke');
     connection.invoke("SendMessage", user, message).catch(function (err) {
 
@@ -82,119 +76,99 @@ document.getElementById("sendButton").addEventListener("click", function (event)
 
 
 
-//document.getElementById("receivedVideo").disabled = true;  //---
 
-//document.getElementById("videoInput").addEventListener("change", async (event) => {
-//    document.getElementById("receivedVideo").disabled = false;
-//    const user = document.getElementById("userInput").value;
-//    const videoFile = event.target.files[0];
-
-//    if (videoFile) {
-
-//        const formData = new FormData();
-//        formData.append("user", user);
-//        formData.append("video", videoFile);
-
-//        await fetch("/uploadVideo", {
-//            method: "POST",
-//            body: formData
-//       });
-//});
+connection.on("Receiveurl", function (image, video, text, userId) {
+    var imageUrl = image;
+    var videoUrl = video;
+    var textcontent = text;
 
 
+    const mediaContainer = document.getElementById("mediaContainer");
+    const timeline = document.querySelector(".timeline");
+
+    function createMediaElement(url, type) {
+        const mediaElement = document.createElement(type);
+        mediaElement.src = url;
+        mediaElement.controls = true;
+        return mediaElement;
+    }
+
+    const textContent = document.getElementById("textContentInput").value;
+
+    const mediaElement = document.createElement("div");
+    mediaElement.className = "media-element";
+
+    if (imageUrl) {
+        const imgElement = document.createElement("img");
+        imgElement.src = imageUrl;
+        imgElement.alt = "Image";
+        mediaElement.appendChild(imgElement);
+    }
+
+    if (videoUrl) {
+        const videoElement = createMediaElement(videoUrl, "video");
+
+        mediaElement.appendChild(videoElement);
+    }
+
+    if (textContent) {
+        const textElement = document.createElement("p");
+        textElement.textContent = textContent;
+        mediaElement.appendChild(textElement);
+    }
+
+    // Like, Dislike, and Comment buttons
+    const likeButton = document.createElement("button");
+    likeButton.textContent = "Like";
+    likeButton.className = "btn btn-secondary";
+    mediaElement.appendChild(likeButton);
+  
 
 
+    const dislikeButton = document.createElement("button");
+    dislikeButton.textContent = "Dislike";
+    dislikeButton.className = "btn btn-secondary";
+    mediaElement.appendChild(dislikeButton);
 
+    const commentInput = document.createElement("input");
+    commentInput.type = "text";
+    commentInput.placeholder = "Enter your comment";
+    commentInput.className = "form-control";
+    mediaElement.appendChild(commentInput);
 
+    const commentButton = document.createElement("button");
+    commentButton.textContent = "Comment";
+    commentButton.className = "btn btn-primary";
+    mediaElement.appendChild(commentButton);
 
-connection.on("Receiveurl", function (user, message) {
-    document.addEventListener("DOMContentLoaded", function () {
-        const mediaContainer = document.getElementById("mediaContainer");
-        const timeline = document.querySelector(".timeline");
+    // Create timeline item
+    const timelineItem = document.createElement("li");
+    timelineItem.className = "timeline-item";
 
-        function createMediaElement(url, type) {
-            const mediaElement = document.createElement(type);
-            mediaElement.src = url;
-            mediaElement.controls = true;
-            return mediaElement;
-        }
+    const timestamp = new Date().toLocaleString();
+    const activityText = imageUrl ? "added an image" : videoUrl ? "added a video" : "posted text content";
 
-        document.getElementById("appendButton").addEventListener("click", function () {
-            const imageUrl = document.getElementById("imageUrlInput").value;
-            const videoUrl = document.getElementById("videoUrlInput").value;
-            const textContent = document.getElementById("textContentInput").value;
-
-            const mediaElement = document.createElement("div");
-            mediaElement.className = "media-element";
-
-            if (imageUrl) {
-                const imgElement = document.createElement("img");
-                imgElement.src = imageUrl;
-                imgElement.alt = "Image";
-                mediaElement.appendChild(imgElement);
-            }
-
-            if (videoUrl) {
-                const videoElement = createMediaElement(videoUrl, "video");
-                mediaElement.appendChild(videoElement);
-            }
-
-            if (textContent) {
-                const textElement = document.createElement("p");
-                textElement.textContent = textContent;
-                mediaElement.appendChild(textElement);
-            }
-
-            // Like, Dislike, and Comment buttons
-            const likeButton = document.createElement("button");
-            likeButton.textContent = "Like";
-            likeButton.className = "btn btn-secondary";
-            mediaElement.appendChild(likeButton);
-
-            const dislikeButton = document.createElement("button");
-            dislikeButton.textContent = "Dislike";
-            dislikeButton.className = "btn btn-secondary";
-            mediaElement.appendChild(dislikeButton);
-
-            const commentInput = document.createElement("input");
-            commentInput.type = "text";
-            commentInput.placeholder = "Enter your comment";
-            commentInput.className = "form-control";
-            mediaElement.appendChild(commentInput);
-
-            const commentButton = document.createElement("button");
-            commentButton.textContent = "Comment";
-            commentButton.className = "btn btn-primary";
-            mediaElement.appendChild(commentButton);
-
-            // Create timeline item
-            const timelineItem = document.createElement("li");
-            timelineItem.className = "timeline-item";
-
-            const timestamp = new Date().toLocaleString();
-            const activityText = imageUrl ? "added an image" : videoUrl ? "added a video" : "posted text content";
-
-            timelineItem.innerHTML = `
+    timelineItem.innerHTML = `
                         <div class="timeline-content">
-                            <p><strong>User</strong> ${activityText} - ${timestamp}</p>
+                            <p><strong>User</strong> ${userId}-${activityText} - ${timestamp}</p>
                         </div>
                     `;
 
-            timeline.appendChild(timelineItem);
+    timeline.appendChild(timelineItem);
 
-            mediaContainer.appendChild(mediaElement);
-        });
-    });
+    mediaContainer.appendChild(mediaElement);
+
 
 
     console.log('Receiveurl Received ');
 
+    console.log('message Received ');
+
     var li = document.createElement("li");
-
-
     document.getElementById("messagesList").appendChild(li);
 
-    li.textContent = `${user} says ${message}`;
+    li.textContent = `${userId}   ${activityText}`;
+
 
     console.log(' Receiveurl completed ');
 });
@@ -202,9 +176,35 @@ connection.on("Receiveurl", function (user, message) {
 
 
 
+connection.on("ReceiveMessagetospecificuser", (userId, sender, message) => {
+    // Handle receiving the message targeted to a specific user
+    // You can display the message in a specific chat window or handle it as needed
+  //  console.log(`Message for ${userId} - ${sender}: ${message}`);
+    console.log('ReceiveMessagetospecificuser');
+
+
+});
 
 
 
+document.getElementById("send-message-button").addEventListener("click", () => {
+    const message = document.getElementById("message-input").value;
+    const userId = document.getElementById("user-id-input").value;
+
+    // Call the server method to send a message to the specified user
+    connection.invoke("SendMessageTospecificUser", userId, message).catch(err => console.error(err));
+
+    // Clear the message input
+    document.getElementById("message-input").value = "";
+});
+
+
+
+connection.on("UpdateConnectedDevicesCount", (connectedDevicesCount) => {
+    // Update the UI to display the updated connected devices count
+    const devicesCountElement = document.getElementById("devices-count");
+    devicesCountElement.textContent = connectedDevicesCount;
+});
 
 
 
@@ -217,7 +217,7 @@ document.getElementById("appendButton").addEventListener("click", function (even
     var text = document.getElementById("textContentInput").value;
 
     console.log('before invoke Sendurltoall');
-    connection.invoke("Sendurltoall", text, image).catch(function (err) {
+    connection.invoke("Sendurltoall", image, video, text).catch(function (err) {
 
 
         console.log('inside function  invoke');
@@ -225,6 +225,137 @@ document.getElementById("appendButton").addEventListener("click", function (even
     });
     console.log('after invoke Sendurltoall');
     event.preventDefault();
+});
+
+
+
+
+connection.on("UserConnected", (userId) => {
+    // Add the user to the list of connected users
+    connectedUsers.push(userId);
+
+    // Update the connected users list
+    updateConnectedUsersList();
+    populateUserSelect();
+    // Display a message indicating that a user has connected
+    const chatWindow = document.getElementById("chat-window");
+    const messageDiv = document.createElement("div");
+    messageDiv.textContent = `${userId} has connected.`;
+    chatWindow.appendChild(messageDiv);
+});
+
+
+
+
+
+
+connection.on("UserDisconnected", (userId) => {
+    // Remove the user from the list of connected users
+    const index = connectedUsers.indexOf(userId);
+    if (index !== -1) {
+        connectedUsers.splice(index, 1);
+    }
+
+    // Update the connected users list
+    updateConnectedUsersList();
+    populateUserSelect();
+    // Display a message indicating that a user has disconnected
+    const chatWindow = document.getElementById("chat-window");
+    const messageDiv = document.createElement("div");
+    messageDiv.textContent = `${userId} has disconnected.`;
+    chatWindow.appendChild(messageDiv);
+});
+
+
+
+
+
+function updateConnectedUsersList() {
+    const userList = document.getElementById("user-list");
+    userList.innerHTML = ""; // Clear the list
+    for (const userId of connectedUsers) {
+        const userItem = document.createElement("li");
+        userItem.textContent = userId;
+        userList.appendChild(userItem);
+    }
+}
+
+
+
+
+
+
+function populateUserSelect() {
+    const selectElement = document.getElementById("private-chat-user");
+    selectElement.innerHTML = '<option value="">Select a user</option>';
+    for (const userId of connectedUsers) {
+        const option = document.createElement("option");
+        option.value = userId;
+        option.textContent = userId;
+        selectElement.appendChild(option);
+    }
+}
+
+
+
+
+
+document.getElementById("open-private-chat-btn").addEventListener("click", () => {
+    const selectedUserId = document.getElementById("private-chat-user").value;
+
+    if (selectedUserId) {
+        openPrivateChatWindow(selectedUserId);
+    }
+});
+
+
+
+
+
+connection.on("UpdateMessageLikes", function (messageId, likeCount) {
+    // Update like count in the UI
+    const likeCountElement = document.getElementById(`like-count-${messageId}`);
+    likeCountElement.textContent = likeCount;
+});
+
+connection.on("UpdateMessageDislikes", function (messageId, dislikeCount) {
+    // Update dislike count in the UI
+    const dislikeCountElement = document.getElementById(`dislike-count-${messageId}`);
+    dislikeCountElement.textContent = dislikeCount;
+});
+
+// Attach event listeners to dynamically created like and dislike buttons
+// Like button event listener
+
+
+
+
+likeButton.addEventListener("click", function () {
+
+    console.log('likeddddddddddddddddd');
+
+    // Handle Like button click
+});
+
+
+
+
+// Dislike button event listener
+dislikeButton.addEventListener("click", function () {
+    // Handle Dislike button click
+    console.log('dislikeddddddddddddddddd');
+
+});
+
+
+
+
+// Comment button event listener
+commentButton.addEventListener("click", function () {
+    const commentText = commentInput.value;
+    // Handle Comment button click with the commentText
+    console.log('cmmented..............');
+
 });
 
 
@@ -241,25 +372,3 @@ document.getElementById("appendButton").addEventListener("click", function (even
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
